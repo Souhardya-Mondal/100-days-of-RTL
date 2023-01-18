ea3c3c6916f73b97057616552b889f5f4aef45da
Souhardya-Mondal <111222241+Souhardya-Mondal@users.noreply.github.com>
2023-01-19 01:14:10 +0530

    Update day4tb.v

--- day4/day4tb.v
+++ day4/day4tb.v
@@ -16,20 +16,23 @@ module tb;
 
   initial
     begin
-      $dumpfile("1.vcd");
+        $dumpfile("1.vcd");
       $dumpvars(0,tb);
+
       $monitor("1st input=%b ,2nd input=%b , operation=%b. Result=%b",a,b,op,out);
 	  
-      #20 a=7;b=10;op=2;
-	    #20 a=2;b=5;op=1;
-      #20 a=7;b=1;op=0;
-      #20 a=7;b=10;op=3;
-      #20 a=20;b=11;op=4;
-      #20 a=12;b=21;op=5;
-      #20 a=62;b=41;op=6;
-      #20 a=72;b=72;op=7;
+      repeat(100) begin
+        #20 a=$random;b=$random;op=$random ;end
+	 
+
+
+      
+      
+	
+     
       #100 $finish;
-      end
+      
+    end
 endmodule
 
   
